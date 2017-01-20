5f8bd96cbc980f3e5c694998e28b30baf43617cf
Mahesha Kulatunga <mkkulatunga1@sheffield.ac.uk>
2017-01-20 10:53:45 +0000

    User authentication done

--- fitsappRnR/app/controllers/messages_controller.rb
+++ fitsappRnR/app/controllers/messages_controller.rb
@@ -1,16 +1,18 @@
 class MessagesController < ApplicationController
   before_action :find_message, only: [:show]
+  before_action :authenticate_user!
+
 
   def index
     @messages = Message.all.order("created_at DESC")
   end
 
   def new
-    @message = Message.new
+    @message = current_user.messages.build
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.build(message_params)
     if @message.save
       redirect_to @message, notice: 'Message sent'
     else
